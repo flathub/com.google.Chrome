057c813bc01911eb337a205cbc44583f3c45af5e
Ryan Gonzalez <rymg19@gmail.com>
2021-07-24 22:08:44 -0500

    Use Cobalt to start Chrome

--- chrome.sh
+++ chrome.sh
@@ -17,38 +17,4 @@ for policy_type in managed recommended; do
   fi
 done
 
-# Determine if to show the first run page & the mimic strategy warning.
-run_stamp="$XDG_DATA_HOME/flatpak-chrome-run-stamp"
-mimic_stamp="$XDG_DATA_HOME/flatpak-chrome-mimic-stamp"
-
-if [[ ! -f "$run_stamp" ]]; then
-  set /app/share/flatpak-chrome/first_run.html chrome://welcome "$@"
-  touch "$run_stamp"
-fi
-
-if [[ ! -f "$mimic_stamp" ]] && ! zypak-helper spawn-strategy-test; then
-  zenity --info --title='Chrome Flatpak' --no-wrap \
-    --text="$(< /app/share/flatpak-chrome/mimic_warning.txt)"
-  touch "$mimic_stamp"
-fi
-
-# Workaround for https://github.com/flathub/org.chromium.Chromium/issues/104
-set -- --disable-features=WebAssemblyTrapHandler "$@"
-
-if [[ -f "$XDG_CONFIG_HOME/chrome-flags.conf" ]]; then
-  IFS=$'\n'
-  flags=($(grep -v '^#' "$XDG_CONFIG_HOME/chrome-flags.conf"))
-  unset IFS
-
-  set -- "${flags[@]}" "$@"
-fi
-
-# Chrome loads cursors by itself, following the standard XCursor search
-# directories. However, the fd.o runtime patches XCursor to look in
-# $XDG_DATA_DIRS, but Chrome's own loading of course does not follow that.
-# Therefore, we manually set the XCursor path to follow $XDG_DATA_DIRS here.
-export XCURSOR_PATH=$(echo "$XDG_DATA_DIRS" | sed 's,\(:\|$\),/icons\1,g')
-export CHROME_WRAPPER=$(readlink -f "$0")
-export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
-
-exec zypak-wrapper.sh /app/extra/chrome "$@"
+exec cobalt "$@"
