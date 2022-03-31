641061725590ecc82718edeccdf7b1768eddc9b1
Ryan Gonzalez <rymg19@gmail.com>
2022-03-31 16:54:38 -0500

    Remove custom policy file
    The dialog on startup to make Chrome the default shows even with this,
    and it breaks Secure DNS settings.
    
    Fixes #43, fixes #97

--- chrome.sh
+++ chrome.sh
@@ -7,10 +7,6 @@ for policy_type in managed recommended; do
   policy_dir="$policy_root/$policy_type"
   mkdir -p "$policy_dir"
 
-  if [[ "$policy_type" == 'managed' ]]; then
-    ln -sf /app/share/flatpak-chrome/flatpak_policy.json "$policy_dir"
-  fi
-
   if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
     find "/run/host/$policy_root/$policy_type" -name '*.json' \
       -exec ln -sf '{}' "$policy_root/$policy_type" \;
